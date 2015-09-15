d2d1e1c943a10abb20c7ef5289230fcc3fbf7908
chinhodado <chin.bimbo@gmail.com>
2015-09-15 00:40:25 -0400

    Update offline database and version

--- src/com/chin/ygodb/YGOSqliteDatabase.java
+++ src/com/chin/ygodb/YGOSqliteDatabase.java
@@ -1,8 +1,8 @@
 package com.chin.ygodb;
 
-import android.content.Context;
-
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import android.content.Context;
 
 /**
  * Helper class for database provisioning
@@ -11,7 +11,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 public class YGOSqliteDatabase extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "ygo.db";
-    public static final int DATABASE_VERSION = 20150911;
+    public static final int DATABASE_VERSION = 20150915;
 
     public YGOSqliteDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
