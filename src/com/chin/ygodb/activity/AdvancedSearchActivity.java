b7f22a0a7e156c11e561fb69de5faf3da4c8cbe6
chinhodado <chin.bimbo@gmail.com>
2015-12-29 17:37:39 -0500

    Use card view in list of advanced search too

--- src/com/chin/ygodb/activity/AdvancedSearchActivity.java
+++ src/com/chin/ygodb/activity/AdvancedSearchActivity.java
@@ -420,7 +420,7 @@ public class AdvancedSearchActivity extends BaseFragmentActivity {
             ListView famListView = (ListView) view.findViewById(R.id.resultListView);
 
             // set the result set of the previous search as the adapter for the list
-            famListView.setAdapter(new CardRegexFilterArrayAdapter(getActivity(), R.layout.list_item, R.id.itemRowText, resultSet));
+            famListView.setAdapter(new CardRegexFilterArrayAdapter(getActivity(), R.layout.list_item_card, R.id.itemRowText, resultSet));
 
             // go to a card's detail page when click on its name on the list
             famListView.setOnItemClickListener(new OnItemClickListener(){
