bb4465605b850a531c3b7141dc48a3512e237137
Trung Do <chin.bimbo@gmail.com>
2019-11-30 22:30:17 -0500

    Switch to Firebase analytics

--- ygodb-module/src/main/java/com/chin/ygodb/YgoDbApplication.java
+++ ygodb-module/src/main/java/com/chin/ygodb/YgoDbApplication.java
@@ -1,7 +1,5 @@
 package com.chin.ygodb;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -9,8 +7,6 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import android.app.Application;
 
 public class YgoDbApplication extends Application {
-    public static GoogleAnalytics analytics;
-    public static Tracker tracker;
 
     @Override
     public void onCreate() {
@@ -27,14 +23,5 @@ public class YgoDbApplication extends Application {
             .diskCacheSize(50 * 1024 * 1024)
             .build();
         ImageLoader.getInstance().init(config);
-
-        // analytics
-        analytics = GoogleAnalytics.getInstance(this);
-        analytics.setLocalDispatchPeriod(1800);
-
-        tracker = analytics.newTracker("UA-38672868-8");
-        tracker.enableExceptionReporting(true);
-        tracker.enableAdvertisingIdCollection(true);
-        tracker.enableAutoActivityTracking(true);
     }
 }
